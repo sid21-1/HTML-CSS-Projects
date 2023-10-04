5e77a7109bd533b7ba76b737cd424dbe552c8860
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-10-04 11:39:54 +0530

    scroll trigger animation

--- Landing page GSAP/script.js
+++ Landing page GSAP/script.js
@@ -22,3 +22,18 @@ t1
         scale:1.3,
         duration:1
     })
+
+gsap.from(".features-section",{
+    scrollTrigger:{
+        trigger:".features-section",
+        scroller:"body",
+        markers:true,
+        start:"top 80%",
+        bottom:" bottom 10%",
+        scrub:true,
+    },
+    opacity:0,
+    scale:1.3,
+    duration:1,
+    delay:1
+})
