49f86cdf1a421667436fd38d79a76c4d4605808b
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-10-04 09:35:34 +0530

    difference between gsap to and from

--- Landing page GSAP/script.js
+++ Landing page GSAP/script.js
@@ -1,4 +1,9 @@
-gsap.to(".header-left h1,p",{
+// gsap.to(".header-left h1,p",{
+//     color:"red",
+//     duration:2
+// })
+
+gsap.from(".header-left h1,p",{
     color:"red",
     duration:2
 })
